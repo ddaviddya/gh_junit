202747056bc7205c359cdeba7892ebea07f407a2
David <ddaviddya@gmail.com>
2023-01-12 18:32:41 -0500

    v2 provocando error en tercer metodo

--- src/test/java/org/example/CuentaTest.java
+++ src/test/java/org/example/CuentaTest.java
@@ -11,7 +11,7 @@ class CuentaTest {
 
     @Test
     void testNombreCuenta(){
-        Cuenta cuenta = new Cuenta("David", new  BigDecimal("1000.12345"));
+        Cuenta cuenta = new Cuenta("David", new  BigDecimal("1000.12345"));//se recomienda pasarlo en cadena
 //        cuenta.setPesona("David");
 
         String esperado = "David";
@@ -19,9 +19,25 @@ class CuentaTest {
         Assertions.assertEquals(esperado, real);
         Assertions.assertTrue(real.equals("David"));
     }
+
     @Test
     void testSaldoCuenta(){
         Cuenta cuenta = new Cuenta("David", new BigDecimal("1000.12345"));
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);//compare -1 0 1
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);//compare -1 0 1
     }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("David Yallerco", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("David Yallerco", new BigDecimal("8900.9997"));
+
+        //comparando por instancia  , osea por referencia
+        assertNotEquals(cuenta2, cuenta); //cuenta valor real , cuenta2 valor esperado
+        assertEquals(cuenta2, cuenta);
+
+    }
+
+
 }
