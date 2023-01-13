13a5c95709e16cf395663a9fbb75e3fe603b95f2
David <ddaviddya@gmail.com>
2023-01-12 19:06:04 -0500

    v6  ok

--- src/main/java/org/example/Cuenta.java
+++ src/main/java/org/example/Cuenta.java
@@ -30,10 +30,10 @@ public class Cuenta {
     }
 
     public  void debito(BigDecimal monto){
-
+        this.saldo = this.saldo.subtract(monto);
     }
-    public void credito(BigDecimal credito){
-
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
     }
 
     @Override
